6f85fcabf5ec8b3b75d865fcffe4f0788bbaba53
unknown <andy.yu@TCIT-RD7.tcit.local>
2016-02-23 17:23:59 +0800

    Add pure css ribbon, improve layout Improve FilterableProductTable component layout on mobile view Update base layout

--- app/components/FilterableProductTable/FilterableProductTable.js
+++ app/components/FilterableProductTable/FilterableProductTable.js
@@ -4,17 +4,6 @@ import ProductTable from './ProductTable';
 import './FilterableProductTable.scss';
 
 class FilterableProductTable extends React.Component {
-	static defaultProps = {
-		products:  [
-		  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
-		  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
-		  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
-		  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
-		  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-		  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
-		]
-	};
-
 	constructor() {
 		super();
 
--- app/components/FullPage/FullPage.js
+++ app/components/FullPage/FullPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import FilterableProductTable from '../FilterableProductTable/FilterableProductTable';
+import FilterableProductTable from '../FilterableProductTable';
 
 import _ from 'lodash';
 import $ from 'jquery';
@@ -9,15 +9,35 @@ import '../../../node_modules/fullpage.js/jquery.fullPage.scss';
 
 
 class FullPage extends React.Component {
+	// Demo data
+	static defaultProps = {
+		filterableProductTableData:  [
+		  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+		  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+		  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+		  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+		  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+		  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+		]
+	};
+
 	render() {
 		return	(
 			<div id="fullpage">
 	    	<div className="section section-intro"><h1>Homepage</h1></div>
-	    	<div className="section"><FilterableProductTable /></div>
 	    	<div className="section">
-	    		<div className="slide"> Slide 1 </div>
-    			<div className="slide"> Slide 2 </div>
-    			<div className="slide"><FilterableProductTable /></div>
+
+       			<div className="ribbon-green"><span>Filterable Table</span></div>
+
+	    		<FilterableProductTable products={this.props.filterableProductTableData} />
+	    	</div>
+	    	<div className="section">
+	    	  <div className="ribbon-wrapper-green">
+       			<div className="ribbon-green"><span>NEWS</span></div>
+       		</div>
+	    		<div className="slide">Slide 1</div>
+    			<div className="slide">Slide 2</div>
+    			<div className="slide">Slide 3</div>
     		</div>
 	    	<div className="section">Some section2</div>
 			</div>
@@ -33,7 +53,8 @@ class FullPage extends React.Component {
 		};
 
 		$('#fullpage').fullpage({
-			sectionsColor: _.values(sectionColors)
+			sectionsColor: _.values(sectionColors),
+			verticalCentered: false
 		});
 	}
 
