6ee9059986044c9f65736d6356d0063b685c6b8a
unknown <andy.yu@TCIT-RD7.tcit.local>
2016-03-23 18:51:16 +0800

    Add some styles

--- app/js/components/Weather/WeatherInfo.js
+++ app/js/components/Weather/WeatherInfo.js
@@ -3,8 +3,42 @@ import React from 'react';
 const WeatherInfo = (props) => {
 	return (
 		<div className="weather__info">
-			<span>today tomorrow</span>
-			<span>recent</span>
+			<section className="weather__info-today-tomorrow">
+				<div className="weather__info-today">
+					<div className="weather-icon">Sunny</div>
+					<span>18°C</span>
+					<span>18:00</span>
+					<span>80%</span>
+				</div>
+				<div className="weather__info-tomorrow">
+					<div className="weather-icon">Sunny</div>
+					<span>18°C</span>
+					<span>18:00</span>
+					<span>80%</span>
+				</div>
+			</section>
+			<section className="weather__info-recent">
+				<ul>
+					<li>
+						<div className="weather-icon">Sunny</div>
+						<div>18°C</div>
+						<div>18:00</div>
+						<div>80%</div>
+					</li>
+					<li>
+						<div className="weather-icon">Sunny</div>
+						<span>18°C</span>
+						<span>18:00</span>
+						<span>80%</span>
+					</li>
+					<li>
+						<div className="weather-icon">Sunny</div>
+						<span>18°C</span>
+						<span>18:00</span>
+						<span>80%</span>
+					</li>
+				</ul>
+			</section>
 		</div>
 	);
 };
